e9cb37d5c40bf80efdccec177087ac6159cc806d
Durvank-cc <prarwankchavan@gmail.com>
2026-07-24 06:42:06 +0000

    changes

---
+++ calculation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a =2 ,b =3;
+    cout<<"sum is ="<<(a+b)<<endl;
+    cout<<"substraction is ="<<(a-b)<<endl;
+    cout<<"multiplecation is ="<<(a*b)<<endl;
+    cout<<"divison is ="<<(a/b)<<endl;
+    cout<<"modulo is ="<<(a%b)<<endl;
+    return 0;
+}
