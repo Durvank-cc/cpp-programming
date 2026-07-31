b45ab2aa73e20c531419bb94f2dc8d7dfe4b8d71
Durvank-cc <prarwankchavan@gmail.com>
2026-07-31 06:27:01 +0000

    naturalnum

---
+++ naturalnum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i = 1;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    cout << "First " << n << " natural numbers are:\n";
+
+    while (i <= n)
+    {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
